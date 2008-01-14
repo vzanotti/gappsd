51dae5a9a27515429c56e260eec9882c9f17a79c
Vincent Zanotti <vzanotti@users.noreply.github.com>
2008-01-14 01:38:23 +0000

    * Adds the reporting module, and its unittests.

---
+++ gappsd/reporting.py
@@ -0,0 +1,323 @@
+#!/usr/bin/python
+#
+# Copyright (C) 2008 Polytechnique.org
+# Author: Vincent Zanotti (vincent.zanotti@polytechnique.org)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Implements the two jobs (ActivityJob and AccountsJob) working with the
+Reporting API of Google Apps."""
+
+import csv
+import datetime
+import google.reporting
+import pytz
+
+import account
+import job
+import logger
+import queue
+from logger import PermanentError, TransientError
+
+class ActivityJob(job.Job):
+  """Implements the 'r_activity' job, which aims at updating the database
+  version of the statistics/metrics offered by the Summary and Activity reports
+  of the Reporting API."""
+
+  _SQL_FIELDS = [
+    "date", "num_accounts", "count_1_day_actives", "count_7_day_actives",
+    "count_14_day_actives", "count_30_day_actives", "count_30_day_idle",
+    "count_60_day_idle", "count_90_day_idle", "usage_in_bytes", "quota_in_mb",
+  ]
+
+  def __init__(self, config, sql, job_dict):
+    job.Job.__init__(self, config, sql, job_dict)
+    self._config = config
+    self._sql = sql
+
+  # Database interaction.
+  def _GetLastReportDate(self):
+    """Returns the date of the last report in the database, or the first day
+    of the backlog if none is available."""
+
+    last_known_report = self._sql.Query(
+      "SELECT UNIX_TIMESTAMP(MAX(date)) AS date FROM gapps_reporting")
+    if last_known_report[0]["date"] is None:
+      report_backlog = self._config.get_int("gappsd.activity-backlog")
+      return datetime.date.today() - datetime.timedelta(report_backlog)
+    return datetime.date.fromtimestamp(last_known_report[0]["date"])
+
+  def _ListDaysToProcess(self, report_limit=None):
+    """Lists the days without activity reports, with a limited backlog.
+    If @p last_report is None, it will get it from the database.
+    """
+
+    latest_report = \
+      GetReportingApiClientInstance(self._config).GetLatestReportDate()
+    if report_limit is None:
+      report = self._GetLastReportDate() + datetime.timedelta(1)
+    else:
+      report = report_limit + datetime.timedelta(1)
+
+    report_list = []
+    while report <= latest_report:
+      report_list.append(report)
+      report += datetime.timedelta(1)
+    return report_list
+
+  def _StoreReport(self, date, report):
+    """Stores the @p activity values in the database, for the @p date."""
+
+    if "date" not in report:
+      report["date"] = date
+    self._sql.Insert("gapps_reporting",
+      dict(filter(lambda (k,v): k in self._SQL_FIELDS, report.items())))
+
+  # Job processing.
+  def RunDailyReport(self, date):
+    """Fetches the activity and summary reports, merges them, and add them to
+    the database."""
+
+    reporting_client = GetReportingApiClientInstance(self._config)
+    activity_reports = reporting_client.GetReport(date, "activity") or []
+    summary_reports = reporting_client.GetReport(date, "summary") or []
+
+    reports = {}
+    for report in activity_reports:
+      if "date" in report:
+        reports[report["date"]] = report
+    for report in summary_reports:
+      if "date" in report:
+        reports.setdefault(report["date"], {}).update(report)
+
+    daily_reports = 0
+    for (report_date, report) in reports.items():
+      self._StoreReport(report_date, report)
+      daily_reports += 1
+    return daily_reports
+
+  def Run(self):
+    """Requests the required daily reports, and run them. Due to the specifities
+    of the Activity and Summary reports, the daily report is run (for a month)
+    on the last day in date_list."""
+
+    last_report = None
+    daily_reports = 0
+
+    while 1:
+      date_list = self._ListDaysToProcess(last_report)
+      if not len(date_list):
+        break
+
+      date = date_list[0]
+      date = max(filter(lambda d: d.year == date.year and d.month == date.month,
+                        date_list))
+      daily_reports += self.RunDailyReport(date)
+      last_report = date
+
+    self.Update(self.STATUS_SUCCESS, "%d days processed" % daily_reports)
+
+
+class AccountsJob(job.Job):
+  """Implementation of the 'r_accounts' job, which updates the database-stored
+  version of the Google Apps accounts, using the Accounts report of the
+  Reporting API."""
+
+  # Format: <reporting field>: [<account field>, <silent synchronization>]
+  _FIELDS = {
+    "account_id":         ["g_account_id",   True],
+    "usage_in_bytes":     ["r_disk_usage",   True],
+    "creation_date":      ["r_creation",     True],
+    "last_login_date":    ["r_last_login",   True],
+    "last_web_mail_date": ["r_last_webmail", True],
+    "surname":            ["g_last_name",    False],
+    "given_name":         ["g_first_name",   False],
+    "suspension_reason":  ["g_suspension",   False],
+  }
+
+  def __init__(self, config, sql, job_dict):
+    job.Job.__init__(self, config, sql, job_dict)
+    self._config = config
+    self._sql = sql
+
+  # SQL Account vs. GApps Account synchronization methods.
+  def SynchronizeSQLAccount(self, sql):
+    """Synchronizes the SQL account based on the fact the account did not
+    show up in the reporting log."""
+
+    if sql["p_status"] != "unprovisioned":
+      queue.CreateQueueJob(self._sql, 'u_sync',
+                           {"username": sql["g_account_name"]})
+
+  def SynchronizeReportingAccount(self, reporting):
+    """Creates a SQL account based on the Reporting version of the account."""
+
+    queue.CreateQueueJob(self._sql, 'u_sync',
+                         {"username": reporting["account_name"]})
+
+  def SynchronizeSQLReportingAccounts(self, sql, reporting):
+    """Synchronizes the SQL version of the account with the reporting version.
+    If the data mismatches, a UserSync job is started (as the reporting version
+    lags at least 12h behind the up-to-date version)."""
+
+    a = account.Account(sql["g_account_name"], sql)
+    for (key, (account_key, silent_update)) in self._FIELDS.items():
+      if key in reporting and a.get(account_key) != reporting[key]:
+        if silent_update:
+          a.set(account_key, reporting[key])
+        else:
+          queue.CreateQueueJob(self._sql, 'u_sync',
+                               {"username": a.get("g_account_name")})
+    a.Update(self._sql)
+
+  # Account list retrieval.
+  def FetchSQLAccounts(self):
+    """Retrieves the list of all Google Apps accounts registered in the
+    database. Returns a username-indexed dictionary of dictionaries."""
+
+    sql_select = ", ".join([
+      "g_account_id", "g_account_name", "g_first_name", "g_last_name",
+      "g_status", "g_suspension", "r_disk_usage",
+      "DATE_FORMAT(r_creation, '%Y%m%d') AS r_creation",
+      "DATE_FORMAT(r_last_login, '%Y%m%d') AS r_last_login",
+      "DATE_FORMAT(r_last_webmail, '%Y%m%d') AS r_last_webmail",
+    ])
+    accounts = self._sql.Query("SELECT %s FROM gapps_accounts" % sql_select)
+    return dict([(account["g_account_name"], account) for account in accounts])
+
+  def FetchReportingAccounts(self):
+    """Retrieves the list of all Google Apps accounts using the Reporting API.
+    Returns a list of dictionaries."""
+
+    reporting_client = GetReportingApiClientInstance(self._config)
+    date = reporting_client.GetLatestReportDate()
+    return reporting_client.GetReport(date, "accounts") or []
+
+  def Run(self):
+    """Retrieves accounts from the two sources to synchronize (SQL and
+    Reporting), and synchronizes each account individually."""
+
+    sql_accounts = self.FetchSQLAccounts()
+    reporting_accounts = self.FetchReportingAccounts()
+    for r_account in reporting_accounts:
+      try:
+        s_account = sql_accounts[r_account["account_name"]]
+        self.SynchronizeSQLReportingAccounts(s_account, r_account)
+        del sql_accounts[r_account["account_name"]]
+      except KeyError:
+        self.SynchronizeReportingAccount(r_account)
+
+    for s_account in sql_accounts.values():
+      self.SynchronizeSQLAccount(s_account)
+
+    self.Update(self.STATUS_SUCCESS)
+
+class ReportingApiClient(google.reporting.ReportRunner):
+  """Implements the compatibility layer between the gappsd framework and the
+  google-provided reporting API client.
+
+  Example usage:
+    reporting = ReportingApiClient(config)
+    reporting.GetReport(datetime.date(2008, 1, 1), "activity")
+    # reporting is an iterable csv object
+  """
+
+  def __init__(self, config):
+    google.reporting.ReportRunner.__init__(self)
+    self.domain = config.get_string("gapps.domain")
+    self.admin_email = \
+      "%s@%s" % (config.get_string("gapps.admin-api-username"), self.domain)
+    self.admin_password = config.get_string("gapps.admin-api-password")
+    self.token_expiration = None
+    self.token_validity = config.get_int("gappsd.token-expiration")
+
+  def _RenewToken(self):
+    """Checks that the token isn't expired yet, and renew it if it is."""
+    if self.token is None or self.token_expiration < datetime.datetime.now():
+      try:
+        logger.info("Reporting API - Requesting authentication token")
+        self.Login()
+        self.token_expiration = \
+          datetime.datetime.now() + datetime.timedelta(0, self.token_validity)
+      except google.reporting.ConnectionError, error:
+        raise TransientError, "ConnectionError: %s" % error
+      except google.reporting.LoginError, error:
+        if str(error) == 'Authentication failure':
+          logger.critical("Reporting API - Authentication refused")
+          raise logger.CredentialError, "Bad credential for Reporting API"
+        raise TransientError, "LoginError: %s" % error
+      logger.info("Reporting API - Authentication success")
+
+  @staticmethod
+  def GetLatestReportDate(now_pst=None):
+    """Returns the date of latest available report. Rule: after 12PM PST,
+    last report is one day ago, otherwise last report is two days ago.
+    Use @p now as current date/time if not None.
+    """
+    if not now_pst:
+      now_pst = datetime.datetime.now(pytz.timezone("America/Los_Angeles"))
+    return now_pst.date() - datetime.timedelta(2 if now_pst.hour < 12 else 1)
+
+  def GetReport(self, date, report_name):
+    """Fetches the report using the reporting API client, handles the client
+    errors, and parses the csv result."""
+    self._RenewToken()
+
+    request = google.reporting.ReportRequest()
+    request.token = self.token
+    request.domain = self.domain
+    request.report_name = report_name
+    request.date = date.strftime("%Y-%m-%d")
+
+    try:
+      report = self.GetReportData(request)
+    except google.reporting.ConnectionError, error:
+      raise TransientError, "ConnectionError: %s" % error
+    except google.reporting.ReportError, error:
+      # Permanent error code requiring admin intervention.
+      if error.reason_code in [1001, 1004, 1005, 1007, 1027]:
+        logger.error("Reporting API - Request failed\n%s" % error)
+        raise PermanentError, "In reporting: %s" % error
+
+      # Temporary error code meaning "report not found".
+      if error.reason_code in [1045, 1059, 1060]:
+        return None
+
+      # Temporary error code meaning "retry".
+      if error.reason_code in [1000, 1011, 1070]:
+        logger.info("Reporting API - Transient report failure\n%s" % error)
+        raise TransientError, "Temporary reporting failure: %s" % error
+
+      # Authentication failure.
+      if error.reason_code == 1006:
+        self.token = None
+        raise TransientError, "Authentication token expired."
+
+      # Unknow error, mail the administrators.
+      logger.error("Reporting API - Unknown error\n%s" % error)
+      raise TransientError, "Unkown error in reporting: %s" % error
+
+    return csv.DictReader(report.split("\n"))
+
+def GetReportingApiClientInstance(config):
+  """Returns the global reporting API client instance, and instantiates it
+  if needed."""
+  global reporting_api_client
+  if reporting_api_client is None:
+    reporting_api_client = ReportingApiClient(config)
+  return reporting_api_client
+
+# Module initialization.
+reporting_api_client = None
+job.job_registry.Register('r_activity', ActivityJob)
+job.job_registry.Register('r_accounts', AccountsJob)
---
+++ testing/reporting.py
@@ -0,0 +1,314 @@
+#!/usr/bin/python
+#
+# Copyright (C) 2008 Polytechnique.org
+# Author: Vincent Zanotti (vincent.zanotti@polytechnique.org)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import datetime
+import gappsd.logger as logger
+import gappsd.reporting as reporting
+import google.reporting as greporting
+import testing.config
+import testing.database
+import time
+import unittest
+
+class MockReportingApiClient(object):
+  """Mock version of the ReportingApiClient, used to test Activity and Accounts
+  jobs. Usage: self.reports contain a sequence of (date, name, result), which
+  are returned to the MockReportingApiClient user. Last element of the sequence
+  is served first.
+  """
+
+  def __init__(self):
+    self.reports = []
+
+  def GetLatestReportDate(self, now_pst=None):
+    return datetime.date(2007, 1, 1)
+
+  def GetReport(self, date, report_name):
+    if not len(self.reports):
+      raise logger.TransientError, \
+        "Unexpected request (%s, %s)" % (date, report_name)
+    if date != self.reports[-1][0]:
+      self.reports.pop()
+      raise logger.TransientError, "Bad report date"
+    if report_name != self.reports[-1][1]:
+      self.reports.pop()
+      raise logger.TransientError, "Bad report name"
+
+    return self.reports.pop()[2]
+
+
+class TestActivityJob(unittest.TestCase):
+  _ACTIVITY_JOB_DATA = {
+    "q_id": 42, "p_status": "active", "p_entry_date": 1200043549,
+    "p_start_date": 1200043559, "j_type": "r_activity", "j_parameters": "{}",
+    "r_softfail_count": 0, "r_softfail_date": 1200043259,
+  }
+
+  def setUp(self):
+    self.config = testing.config.MockConfig()
+    self.sql = testing.database.MockSQL()
+    self.activity = \
+      reporting.ActivityJob(self.config, self.sql, self._ACTIVITY_JOB_DATA)
+    reporting.reporting_api_client = MockReportingApiClient()
+
+  def testGetLastReportDate(self):
+    self.sql.query_result = [{"date": None}]
+    self.assertEquals(self.activity._GetLastReportDate(),
+                      datetime.date.today() - datetime.timedelta(30))
+
+    self.sql.query_result = [{"date": 1167608563}]
+    self.assertEquals(self.activity._GetLastReportDate(),
+                      datetime.date(2007, 1, 1))
+
+  def testListDaysToProcess(self):
+    yesterday = datetime.date(2007, 1, 1)
+    two_days_ago = yesterday - datetime.timedelta(1)
+    self.sql.query_result = [{"date": 1167435763}]
+
+    two_days = self.activity._ListDaysToProcess()
+    one_day = self.activity._ListDaysToProcess(two_days_ago)
+
+    self.assertEquals(two_days, [two_days_ago, yesterday])
+    self.assertEquals(one_day, [yesterday])
+
+  def testRunDailyReport(self):
+    date20070101 = datetime.date(2007, 1, 1)
+    reporting.reporting_api_client.reports = [
+      (date20070101, "summary", [{"date": "20070101", "quota_in_mb": 69}]),
+      (date20070101, "activity", [{"date": "20070101", "usage_in_bytes": 42}]),
+    ]
+
+    self.assertEquals(self.activity.RunDailyReport(date20070101), 1)
+    self.assertEquals(self.sql.insert_values, {
+      "date": "20070101", "usage_in_bytes": 42, "quota_in_mb": 69
+    })
+
+  def testRun(self):
+    yesterday = datetime.date(2007, 1, 1)
+    two_days_ago = yesterday - datetime.timedelta(1)
+    reporting.reporting_api_client.reports = [
+      (yesterday, "summary", [{"date": "20070102", "usage_in_bytes": 43}]),
+      (yesterday, "activity", None),
+    ]
+    self.sql.query_result = [{"date": 1167522163}]
+
+    self.activity.Run()
+    self.assertEquals(self.sql.insert_values, {
+      "date": "20070102", "usage_in_bytes": 43
+    })
+
+
+class TestAccountsReport(unittest.TestCase):
+  _ACCOUNTS_JOB_DATA = {
+    "q_id": 42, "p_status": "active", "p_entry_date": 1200043549,
+    "p_start_date": 1200043559, "j_type": "r_activity", "j_parameters": "{}",
+    "r_softfail_count": 0, "r_softfail_date": 1200043259,
+  }
+  _ACCOUNT_DICT = {
+    "g_account_name": "foo.bar", "g_account_id": "0000000042424242",
+    "g_first_name": "foo", "g_last_name": "bar", "r_disk_usage": 42,
+    "r_creation": "20070101", "r_last_login": "20070101",
+    "r_last_webmail": "20070101", "g_suspension": None,
+  }
+  
+  # Methods to be injected in the AccountsJob object.
+  class SQLUsed(Exception):
+    pass
+  class ReportingUsed(Exception):
+    pass
+  class SQLReportingUsed(Exception):
+    pass
+  
+  def SynchronizeSQLAccount(self, sql):
+    raise self.SQLUsed
+  def SynchronizeReportingAccount(self, reporting):
+    raise self.ReportingUsed
+  def SynchronizeSQLReportingAccounts(self, sql, reporting):
+    raise self.SQLReportingUsed
+
+  # Tests.
+  def setUp(self):
+    self.config = testing.config.MockConfig()
+    self.sql = testing.database.MockSQL()
+    self.accounts = \
+      reporting.AccountsJob(self.config, self.sql, self._ACCOUNTS_JOB_DATA)
+    reporting.reporting_api_client = MockReportingApiClient()
+
+  def testSynchronizeSQLAccount(self):
+    self.sql.insert_result = None
+    self.accounts.SynchronizeSQLAccount({
+      "g_account_name": "foo.bar",
+      "p_status": "active",
+    })
+    self.assertEquals(self.sql.insert_values["j_parameters"],
+                      '{"username": "foo.bar"}')
+
+    self.sql.insert_values = None
+    self.accounts.SynchronizeSQLAccount({
+      "g_account_name": "qux.quz",
+      "p_status": "unprovisioned",
+    })
+    self.assertEquals(self.sql.insert_values, None)
+
+  def testSynchronizeReportingAccount(self):
+    self.accounts.SynchronizeReportingAccount({
+      "account_name": "foo.bar",
+      "creation_date": "20070101",
+    })
+    self.assertEquals(self.sql.insert_values["j_parameters"],
+                      '{"username": "foo.bar"}')
+
+  def testSynchronizeSQLReportingAccounts(self):
+    self.sql.insert_values = None
+    self.accounts.SynchronizeSQLReportingAccounts(self._ACCOUNT_DICT,
+      {"usage_in_bytes": 69, "last_login_date": "20070101"})
+    self.assertEquals(self.sql.insert_values, None)
+    self.assertEquals(self.sql.update_values["r_disk_usage"], 69)
+    
+    self.accounts.SynchronizeSQLReportingAccounts(self._ACCOUNT_DICT,
+      {"surname": "qux", "last_web_mail_date": "20070102"})
+    self.assertEquals(self.sql.insert_values["j_parameters"],
+                      '{"username": "foo.bar"}')
+    self.assertEquals(self.sql.update_values["r_last_webmail"], "20070102")
+
+  def testFetchSQLAccounts(self):
+    self.sql.query_query = None
+    self.sql.query_result = [{"g_account_name": "foo.bar"}]
+    a = self.accounts.FetchSQLAccounts()
+    self.assertEquals(a, {"foo.bar": {"g_account_name": "foo.bar"}})
+
+  def testFetchReportingAccounts(self):
+    reporting.reporting_api_client.reports = [
+      (datetime.date(2007, 1, 1), "accounts", [])
+    ]
+    self.accounts.FetchReportingAccounts()
+
+  def testRun(self):
+    self.accounts.SynchronizeSQLAccount = self.SynchronizeSQLAccount
+    self.accounts.SynchronizeReportingAccount = self.SynchronizeReportingAccount
+    self.accounts.SynchronizeSQLReportingAccounts = \
+      self.SynchronizeSQLReportingAccounts
+    
+    self.sql.query_result = [{"g_account_name": "foo.bar"}]
+    reporting.reporting_api_client.reports = [
+      (datetime.date(2007, 1, 1), "accounts", [{"account_name": "foo.bar"}])
+    ]
+    self.assertRaises(self.SQLReportingUsed, self.accounts.Run)
+    
+    self.sql.query_result = [{"g_account_name": "qux.quz"}]
+    reporting.reporting_api_client.reports = [
+      (datetime.date(2007, 1, 1), "accounts", [])
+    ]
+    self.assertRaises(self.SQLUsed, self.accounts.Run)
+
+    self.sql.query_result = []
+    reporting.reporting_api_client.reports = [
+      (datetime.date(2007, 1, 1), "accounts", [{"account_name": "foo.bar"}])
+    ]
+    self.assertRaises(self.ReportingUsed, self.accounts.Run)
+
+
+class TestReportingApiClient(unittest.TestCase):
+  # Redefinition of google.reporting.ReportRunner methods.
+  def Login(self):
+    if self.reporting.admin_email is None or \
+       self.reporting.admin_password is None:
+      raise greporting.LoginError("TestReporting: email or password undefined.")
+    if self.login_result is None:
+      return None
+    raise self.login_result
+
+  def GetReportData(self, request):
+    self.request = request
+    if type(self.request_result) is str:
+      return self.request_result
+    raise self.request_result
+
+  # Tests.
+  def setUp(self):
+    self.login_result = None
+    self.request = None
+    self.request_result = None
+
+    self.reporting = reporting.ReportingApiClient(testing.config.MockConfig())
+    self.reporting.Login = self.Login
+    self.reporting.GetReportData = self.GetReportData
+
+  def testRenewTokenNotExpired(self):
+    self.login_result = greporting.ConnectionError("", "")
+    self.reporting.token = "token"
+    self.reporting.token_expiration = \
+      datetime.datetime.now() + datetime.timedelta(1)
+    self.reporting._RenewToken()
+
+  def testRenewTokenExpiredNormal(self):
+    self.login_result = None
+    self.reporting.token = "token"
+    self.reporting.token_expiration = \
+      datetime.datetime.now() - datetime.timedelta(0, 1)
+    self.reporting._RenewToken()
+
+  def testRenewTokenError(self):
+    self.login_result = greporting.ConnectionError("", "")
+    self.assertRaises(logger.TransientError, self.reporting._RenewToken)
+
+    self.login_result = greporting.LoginError('Authentication failure')
+    self.assertRaises(logger.CredentialError, self.reporting._RenewToken)
+
+    self.login_result = greporting.LoginError('Foo')
+    self.assertRaises(logger.TransientError, self.reporting._RenewToken)
+
+  def testGetLatestReportDate(self):
+    date20061231 = datetime.date(2006, 12, 31)
+    date20070101 = datetime.date(2007, 1, 1)
+    self.assertEquals(reporting.ReportingApiClient.GetLatestReportDate(
+      datetime.datetime(2007, 1, 2, 11, 59, 59)), date20061231)
+    self.assertEquals(reporting.ReportingApiClient.GetLatestReportDate(
+      datetime.datetime(2007, 1, 2, 12, 0, 1)), date20070101)
+
+  def testGetReport(self):
+    self.request_result = "blih\n1\n2"
+    self.reporting.token = "token"
+    self.reporting.token_expiration = \
+      datetime.datetime.now() - datetime.timedelta(0, 1)
+    r = self.reporting.GetReport(datetime.date(2006, 12, 31), 'activity')
+
+    self.assertEquals(self.request.domain, "GD")
+    self.assertEquals(self.request.token, "token")
+    self.assertEquals(self.request.report_name, "activity")
+    self.assertEquals(self.request.date, "2006-12-31")
+    self.assertEquals([l for l in r], [{"blih": "1"}, {"blih": "2"}])
+
+  def testGetReportErrors(self):
+    self.request_result = greporting.ReportError()
+    self.request_result.reason_code = 1001
+    self.assertRaises(logger.PermanentError, self.reporting.GetReport,
+                      datetime.date(2006, 12, 31), 'activity')
+
+    self.request_result.reason_code = 1006
+    self.assertRaises(logger.TransientError, self.reporting.GetReport,
+                      datetime.date(2006, 12, 31), 'activity')
+    self.assertEquals(self.reporting.token, None)
+
+    self.request_result = greporting.ConnectionError("", "")
+    self.assertRaises(logger.TransientError, self.reporting.GetReport,
+                      datetime.date(2006, 12, 31), 'activity')
+
+  def testGetAbsentReport(self):
+    self.request_result = greporting.ReportError()
+    self.request_result.reason_code = 1045
+    self.reporting.GetReport(datetime.date(2006, 12, 31), 'activity')
