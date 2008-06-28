e2c0d3e63a9e85f08d5db142a47f1c1617684636
Vincent Zanotti <vzanotti@users.noreply.github.com>
2008-06-28 21:50:36 +0000

    Updates tests in provisioning to the new Mox framework; cleans up the tests.

--- testing/provisioning.py
+++ testing/provisioning.py
@@ -15,77 +15,16 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-"""TODO"""
+"""Tests for the provisioning features of the GAppsd tools."""
 
 import gappsd.job as job
 import gappsd.logger as logger
 import gappsd.provisioning as provisioning
 import gdata.apps.service
 import gdata.service
+import mox
 import testing.config
 import unittest
-
-class MockGDataService(object):
-  """Mocks gdata.apps.service.AppsService, so as to test the
-  provisioning.ProvisioningApiClient class."""
-
-  def __init__(self):
-    self.auth_token = None
-    self.captcha_url = ""
-    self.programmatic_login_answer = None
-
-    self.create_user_answer = None
-    self.create_user_parameters = ()
-    self.retrieve_user_answer = None
-    self.retrieve_user_parameters = ()
-    self.update_user_answer = None
-    self.update_user_parameters = ()
-    self.delete_user_answer = None
-    self.delete_user_parameters = ()
-
-  def ProgrammaticLogin(self):
-    if isinstance(self.programmatic_login_answer, Exception):
-      raise self.programmatic_login_answer
-    return self.programmatic_login_answer
-
-  def CreateUser(self, user_name, family_name, given_name,
-                 password, suspended='false', password_hash_function=None):
-    self.create_user_parameters = (user_name, family_name, given_name,
-      password, suspended, password_hash_function)
-    if isinstance(self.create_user_answer, Exception):
-      raise self.create_user_answer
-    return self.create_user_answer
-
-  def RetrieveUser(self, user_name):
-    self.retrieve_user_parameters = user_name
-    if isinstance(self.retrieve_user_answer, Exception):
-      raise self.retrieve_user_answer
-    return self.retrieve_user_answer
-
-  def UpdateUser(self, user_name, user_entry):
-    self.update_user_parameters = (user_name, user_entry)
-    if isinstance(self.update_user_answer, Exception):
-      raise self.update_user_answer
-    return self.update_user_answer
-
-  def DeleteUser(self, user_name):
-    self.delete_user_parameters = user_name
-    if isinstance(self.delete_user_answer, Exception):
-      raise self.delete_user_answer
-    return self.delete_user_answer
-
-class MockProvisioningApiClient(provisioning.ProvisioningApiClient):
-  def __init__(self):
-    self.service = MockGDataService()
-    provisioning.ProvisioningApiClient(
-      testing.config.MockConfig(),
-      self.service)
-
-  @staticmethod
-  def PrepareGlobalMock():
-    global provisioning_api_client
-    provisioning_api_client = MockProvisioningApiClient()
-
 
 class TestUserJob(unittest.TestCase):
   _JOB_DATA = {
@@ -95,43 +34,41 @@ class TestUserJob(unittest.TestCase):
   }
 
   def setUp(self):
-    MockProvisioningApiClient.PrepareGlobalMock()
     self.config = testing.config.MockConfig()
-    self.sql = testing.database.MockSQL()
 
   def testCheckParametersUsername(self):
     self._JOB_DATA['j_parameters'] = '{"username": "abcd"}'
-    provisioning.UserJob(self.config, self.sql, self._JOB_DATA)
+    provisioning.UserJob(self.config, None, self._JOB_DATA)
 
     self._JOB_DATA['j_parameters'] = '{"username": "#"}'
     self.assertRaises(job.JobContentError, provisioning.UserJob,
-                      self.config, self.sql, self._JOB_DATA)
+                      self.config, None, self._JOB_DATA)
 
     self._JOB_DATA['j_parameters'] = '{}'
     self.assertRaises(job.JobContentError, provisioning.UserJob,
-                      self.config, self.sql, self._JOB_DATA)
+                      self.config, None, self._JOB_DATA)
 
   def testCheckParametersFirstName(self):
     self._JOB_DATA['j_parameters'] = \
       '{"first_name": "beno\uC3AFt", "username": "foo.bar"}'
-    provisioning.UserJob(self.config, self.sql, self._JOB_DATA)
+    provisioning.UserJob(self.config, None, self._JOB_DATA)
 
     self._JOB_DATA['j_parameters'] = \
       '{"first_name": "#", "username": "foo.bar"}'
     self.assertRaises(job.JobContentError, provisioning.UserJob,
-                      self.config, self.sql, self._JOB_DATA)
+                      self.config, None, self._JOB_DATA)
 
   def testCheckParametersLastName(self):
     self._JOB_DATA['j_parameters'] = \
       '{"last_name": "beno\uC3AFt", "username": "foo.bar"}'
-    provisioning.UserJob(self.config, self.sql, self._JOB_DATA)
+    provisioning.UserJob(self.config, None, self._JOB_DATA)
 
     self._JOB_DATA['j_parameters'] = \
       '{"last_name": "#", "username": "foo.bar"}'
     self.assertRaises(job.JobContentError, provisioning.UserJob,
-                      self.config, self.sql, self._JOB_DATA)
+                      self.config, None, self._JOB_DATA)
 
-class TestUserCreateJob(unittest.TestCase):
+class TestUserCreateJob(mox.MoxTestBase):
   _JOB_DATA = {
     "q_id": 42, "p_status": "active", "p_entry_date": 1200043549,
     "p_start_date": 1200043559, "j_type": "r_activity",
@@ -140,110 +77,108 @@ class TestUserCreateJob(unittest.TestCase):
   }
 
   def setUp(self):
+    mox.MoxTestBase.setUp(self)
+    self.client = self.mox.CreateMock(provisioning.ProvisioningApiClient)
     self.config = testing.config.MockConfig()
-    self.service = MockGDataService()
-    self.sql = testing.database.MockSQL()
-    provisioning.provisioning_api_client = \
-      provisioning.ProvisioningApiClient(self.config, self.service)
+    provisioning.provisioning_api_client = self.client
 
   def testCreateExistingAccount(self):
-    self.service.retrieve_user_answer = True
-    j = provisioning.UserCreateJob(self.config, self.sql, self._JOB_DATA)
+    self.client.TryRetrieveUser("foo.bar").AndReturn(True)
+    self.mox.ReplayAll()
+
+    j = provisioning.UserCreateJob(self.config, None, self._JOB_DATA)
     self.assertRaises(logger.PermanentError, j.Run)
-    self.assertEquals(self.service.retrieve_user_parameters, "foo.bar")
 
   def testCreateAccount(self):
     # TODO
     pass
 
-class TestUserDeleteJob(unittest.TestCase):
+class TestUserDeleteJob(mox.MoxTestBase):
   def setUp(self):
+    mox.MoxTestBase.setUp(self)
+    self.client = self.mox.CreateMock(provisioning.ProvisioningApiClient)
     self.config = testing.config.MockConfig()
-    self.service = MockGDataService()
-    self.sql = testing.database.MockSQL()
-    provisioning.provisioning_api_client = \
-      provisioning.ProvisioningApiClient(self.config, self.service)
+    provisioning.provisioning_api_client = self.client
 
   # TODO
 
-class TestUserSynchronizeJob(unittest.TestCase):
+class TestUserSynchronizeJob(mox.MoxTestBase):
   def setUp(self):
+    mox.MoxTestBase.setUp(self)
+    self.client = self.mox.CreateMock(provisioning.ProvisioningApiClient)
     self.config = testing.config.MockConfig()
-    self.service = MockGDataService()
-    self.sql = testing.database.MockSQL()
-    provisioning.provisioning_api_client = \
-      provisioning.ProvisioningApiClient(self.config, self.service)
+    provisioning.provisioning_api_client = self.client
 
   # TODO
 
-class TestUserUpdateJob(unittest.TestCase):
+class TestUserUpdateJob(mox.MoxTestBase):
   def setUp(self):
+    mox.MoxTestBase.setUp(self)
+    self.client = self.mox.CreateMock(provisioning.ProvisioningApiClient)
     self.config = testing.config.MockConfig()
-    self.service = MockGDataService()
-    self.sql = testing.database.MockSQL()
-    provisioning.provisioning_api_client = \
-      provisioning.ProvisioningApiClient(self.config, self.service)
+    provisioning.provisioning_api_client = self.client
 
   # TODO
 
-class TestProvisioningApiClient(unittest.TestCase):
+class TestProvisioningApiClient(mox.MoxTestBase):
   def setUp(self):
-    self.service = MockGDataService()
+    mox.MoxTestBase.setUp(self)
+    self.service = self.mox.CreateMock(gdata.apps.service.AppsService)
     self.client = provisioning.ProvisioningApiClient(
       testing.config.MockConfig(),
       self.service)
 
   def testRenewToken(self):
-    self.service.programmatic_login_answer = None
+    self.service.ProgrammaticLogin()
+    self.service.ProgrammaticLogin().AndRaise(gdata.service.BadAuthentication)
+    self.service.ProgrammaticLogin().AndRaise(gdata.service.CaptchaRequired)
+    self.service.ProgrammaticLogin().AndRaise(gdata.service.Error)
+    self.service.ProgrammaticLogin().AndRaise(Exception)
+    self.mox.ReplayAll()
+
     self.client._RenewToken()
-
-    self.service.programmatic_login_answer = gdata.service.BadAuthentication()
     self.assertRaises(logger.CredentialError, self.client._RenewToken)
-
-    self.service.programmatic_login_answer = gdata.service.CaptchaRequired()
     self.assertRaises(logger.CredentialError, self.client._RenewToken)
-
-    self.service.programmatic_login_answer = gdata.service.Error()
     self.assertRaises(logger.TransientError, self.client._RenewToken)
-
-    self.service.programmatic_login_answer = Exception()
     self.assertRaises(logger.TransientError, self.client._RenewToken)
 
   def testUpdateUser(self):
-    self.service.update_user_answer = True
-    self.assertEquals(True, self.client.UpdateUser("foo.bar", None))
+    kException42 = gdata.apps.service.AppsForYourDomainException(
+        {"status": "42"})
+    kException42.error_code = gdata.apps.service.UNKOWN_ERROR
+    kException200 = gdata.apps.service.AppsForYourDomainException(
+        {"status": "200"})
+    kException200.error_code = gdata.apps.service.ENTITY_DOES_NOT_EXIST
+    kException200.reason = ""
 
-    self.service.update_user_answer = \
-      gdata.apps.service.AppsForYourDomainException({"status": "42"})
-    self.service.update_user_answer.error_code = gdata.apps.service.UNKOWN_ERROR
+    self.service.UpdateUser("foo.bar", None).AndReturn(True)
+    self.service.UpdateUser("foo.bar", None).AndRaise(kException42)
+    self.service.UpdateUser("foo.bar", None).AndRaise(kException200)
+    self.service.UpdateUser("foo.bar", None).AndRaise(Exception)
+    self.mox.ReplayAll()
+
+    self.assertEquals(True, self.client.UpdateUser("foo.bar", None))
     self.assertRaises(logger.TransientError, self.client.UpdateUser,
                       "foo.bar", None)
-
-    self.service.update_user_answer = \
-      gdata.apps.service.AppsForYourDomainException({"status": "200"})
-    self.service.update_user_answer.error_code = \
-      gdata.apps.service.ENTITY_DOES_NOT_EXIST
-    self.service.update_user_answer.reason = ""
     self.assertRaises(logger.PermanentError, self.client.UpdateUser,
                       "foo.bar", None)
-
-    self.service.update_user_answer = Exception()
     self.assertRaises(Exception, self.client.UpdateUser, "foo.bar", None)
 
   def testTryUpdateUser(self):
-    self.service.update_user_answer = True
+    kExceptionEntity = gdata.apps.service.AppsForYourDomainException(
+        {"status": "42"})
+    kExceptionEntity.error_code = gdata.apps.service.ENTITY_DOES_NOT_EXIST
+    kExceptionUser = gdata.apps.service.AppsForYourDomainException(
+        {"status": "200"})
+    kExceptionUser.error_code = gdata.apps.service.USER_SUSPENDED
+    kExceptionUser.reason = ""
+
+    self.service.UpdateUser("foo.bar", None).AndReturn(True)
+    self.service.UpdateUser("foo.bar", None).AndRaise(kExceptionEntity)
+    self.service.UpdateUser("foo.bar", None).AndRaise(kExceptionUser)
+    self.mox.ReplayAll()
+
     self.assertEquals(True, self.client.TryUpdateUser("foo.bar", None))
-
-    self.service.update_user_answer = \
-      gdata.apps.service.AppsForYourDomainException({"status": "200"})
-    self.service.update_user_answer.error_code = \
-      gdata.apps.service.ENTITY_DOES_NOT_EXIST
     self.assertEquals(None, self.client.TryUpdateUser("foo.bar", None))
-
-    self.service.update_user_answer = \
-      gdata.apps.service.AppsForYourDomainException({"status": "200"})
-    self.service.update_user_answer.error_code = \
-      gdata.apps.service.USER_SUSPENDED
-    self.service.update_user_answer.reason = ""
     self.assertRaises(logger.PermanentError, self.client.TryUpdateUser,
                       "foo.bar", None)
